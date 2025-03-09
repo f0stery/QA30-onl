7310f9206a922ea1261eaa5b2ffd9ed045603c0e
f0ster <f0rsteryo@gmail.com>
2025-03-09 14:14:45 +0300

    Task 2

--- src/homework_3/HomeWork.java
+++ src/homework_3/HomeWork.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class HomeWork {
     public static void main(String[] args){
-    getNameSeason();
+    // Methods
+        getNameSeasonIfElse();
     }
 
-    public static void getNameSeason(){
+    public static void getNameSeasonSwitch(){
         Scanner input = new Scanner(System.in);
         System.out.print("Please, enter the month number to find out the season: ");
         int month = input.nextInt();
@@ -28,4 +29,23 @@ public class HomeWork {
                 System.out.println("There is no such month in a year");
         }
     }
+
+    public static void getNameSeasonIfElse(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please, enter the month number to find out the season: ");
+        int month = input.nextInt();
+
+        if (month <= 2 && month > 0 || month == 12){
+            System.out.println("It's Winter!");
+        } else if (month <= 5 && month >= 3){
+            System.out.println("It's Spring!");
+        } else if (month <= 8 && month >= 6){
+            System.out.println("It's Summer!");
+        } else if (month <= 11 && month >= 9){
+            System.out.println("It's Fall!");
+        } else {
+            System.out.println("There is no such month in a year");
+        }
+    }
+
 }
