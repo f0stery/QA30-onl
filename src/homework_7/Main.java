0e399d689392d9fd325aaeea8aaeb0a7ca469924
f0ster <f0rsteryo@gmail.com>
2025-03-30 21:25:44 +0300

    homework7_1

---
+++ src/homework_7/Main.java
@@ -0,0 +1,35 @@
+package homework_7;
+
+public class Main {
+    public static void main(String[] args){
+
+        Phone myPhone = new Phone("+375(29)-515-23-21", "iPhone 15 pro", 187);
+        Phone phone14 = new Phone("+375(29)-612-23-33", "iPhone 14");
+        Phone phone13 = new Phone();
+
+        phone14.setWeight(200);
+
+        phone13.setNumber("555-555");
+        phone13.setModel("iPhone 13");
+        phone13.setWeight(240);
+
+        System.out.println(myPhone);
+        System.out.println();
+
+        System.out.println(phone14);
+        System.out.println();
+
+        System.out.println(phone13);
+        System.out.println();
+
+
+        phone13.receiveCall("Evgeny");
+        phone13.receiveCall("Bob", "+375(29)232-23-23");
+        phone13.receiveCall("Liza");
+
+        System.out.println();
+
+        myPhone.sendMessage("111-111", "222-222", "333-333");
+
+    }
+}
---
+++ src/homework_7/Phone.java
@@ -0,0 +1,69 @@
+package homework_7;
+
+public class Phone {
+    private String number;
+    private String model;
+    private float weight;
+
+
+    public Phone(String number, String model, float weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(){
+    }
+
+    public void receiveCall(String callerName) {
+        System.out.println(callerName + " is calling!");
+    }
+
+    public void receiveCall(String callerName, String callerNumber) {
+        System.out.println(callerName + "is calling from " + callerNumber);
+    }
+
+    public void sendMessage(String... phoneNumbers) {
+        System.out.println("Sending message to: ");
+        for (String num : phoneNumbers) {
+            System.out.println(num);
+        }
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public float getWeight(){
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone Information: " + "\n" +
+                "Number: " + getNumber() + "\n" +
+                "Model: " + getModel() + "\n" +
+                "Weight: " + getWeight();
+    }
+}
